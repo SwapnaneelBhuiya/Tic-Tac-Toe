36403d251457c78d88ffcfe3c22821f968e64b7f
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-07 10:50:30 +0530

    pushing into master

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -4,15 +4,12 @@ import java.util.*;
 public class TicTacToeGame {
 	public static void main(String args[]) {
 		System.out.println("Welcome to the tictactoe problem");
+		Scanner sc = new Scanner(System.in);
 		char board[] = createArray();
-		char player = chooseCharacter();
-		char computer;
-		if (player == 'X')
-			computer = 'O';
-		else
-			computer = 'X';
+		char player = chooseCharacter(sc);
+		char computer=(player=='X')?'O' : 'X';
+		validBoardCells(board);
 	}
-
 	// initialize char array for the game
 	public static char[] createArray() {
 		char board[] = new char[10];
@@ -22,13 +19,21 @@ public class TicTacToeGame {
 		return board;
 	}
 	//take the character to be played by user as input
-	public static char chooseCharacter() {
-		Scanner sc = new Scanner(System.in);
+	public static char chooseCharacter(Scanner sc) {
 		System.out.println("Enter the character to be chosen by user");
 		char ch = sc.next().charAt(0);
 		sc.nextLine();
 		String temp = "" + ch;
 		temp.toUpperCase();
 		return temp.charAt(0);
+	}
+	//display valid cells to help the user make his move
+	public static void validBoardCells(char board[])
+	{
+		System.out.println(board[1]+" "+board[2]+" "+board[3]);
+		System.out.println("                                ");
+		System.out.println(board[4]+" "+board[5]+" "+board[6]);
+		System.out.println("                                ");
+		System.out.println(board[7]+" "+board[8]+" "+board[9]);
 	}
 }

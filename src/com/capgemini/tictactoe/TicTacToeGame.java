2a3fd0292dec3996bec0b866b84b5e46781e1b1e
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-07 11:37:49 +0530

    [swapnaneel]UC4:Take user input to populate the board

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,5 +1,4 @@
 package com.capgemini.tictactoe;
-
 import java.util.*;
 public class TicTacToeGame {
 	public static void main(String args[]) {
@@ -7,7 +6,10 @@ public class TicTacToeGame {
 		Scanner sc = new Scanner(System.in);
 		char board[] = createArray();
 		char player = chooseCharacter(sc);
-		char computer=(player=='X')?'O' : 'X';
+		char computer = (player == 'X') ? 'O' : 'X';
+		System.out.println("Character chosen by computer= "+computer);
+		board=userInput(board,sc,player);
+		System.out.println("After user entry the board looks like");
 		validBoardCells(board);
 	}
 	// initialize char array for the game
@@ -18,7 +20,7 @@ public class TicTacToeGame {
 		}
 		return board;
 	}
-	//take the character to be played by user as input
+	// take the character to be played by user as input
 	public static char chooseCharacter(Scanner sc) {
 		System.out.println("Enter the character to be chosen by user");
 		char ch = sc.next().charAt(0);
@@ -27,13 +29,26 @@ public class TicTacToeGame {
 		temp.toUpperCase();
 		return temp.charAt(0);
 	}
-	//display valid cells to help the user make his move
-	public static void validBoardCells(char board[])
-	{
-		System.out.println(board[1]+" "+board[2]+" "+board[3]);
+	// display valid cells to help the user make his move
+	public static void validBoardCells(char board[]) {
+		System.out.println(board[1] + " " + board[2] + " " + board[3]);
 		System.out.println("                                ");
-		System.out.println(board[4]+" "+board[5]+" "+board[6]);
+		System.out.println(board[4] + " " + board[5] + " " + board[6]);
 		System.out.println("                                ");
-		System.out.println(board[7]+" "+board[8]+" "+board[9]);
+		System.out.println(board[7] + " " + board[8] + " " + board[9]);
+	}
+	// user input into the board
+	public static char[] userInput(char board[], Scanner sc, char player) {
+		while (true) {
+			System.out.println("Enter the position that you want to play");
+			int pos = sc.nextInt();
+			if (board[pos] != ' ')
+				System.out.println("Enter again as position is filled");
+			else {
+				board[pos] = player;
+				break;
+			}
+		}
+		return board;
 	}
 }
